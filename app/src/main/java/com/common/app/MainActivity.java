770f7a59599f4b81cd8130d2a3b8e5cd913709e9
唐锐 <shiner0207@163.com>
2021-07-29 16:13:16 +0800

    设备信息

---
+++ app/src/main/java/com/common/app/MainActivity.java
@@ -0,0 +1,16 @@
+package com.common.app;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.common.tools.AdbTools;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
